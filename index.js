28abe78268d3ade723f8b21222a1d7fe6107b0f0
Nishan <baralnishan987@gmail.com>
2022-09-16 23:49:45 +0900

    adding start script

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.use((err,req,res,next)=>{
     if(!err.message) err.message= 'Something Went Wrong'
     return res.status(status).send(`error ${err.message}`)
 })
-
-app.listen(3000, ()=>{
-    console.log('Connection established at port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`Connection established at port ${port}`)
 })
